740f6401e8176d88a46f34a29ac6bd1e98f7c95d
Andrey Melentyev <andrey.melentyev@gmail.com>
2014-11-21 12:47:11 +0300

    Completed step 4 - Appropriately labels the data set with descriptive variable names.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
+## Variable Names
+
+* According to `features_info.txt` the t-prefix was expanded to time, f-prefix was expanded to freq.
+* Acc became Accelerometer
+* Gyro became Gyroscope
+* Redundant special characters were removed so the variables are safe to use in R code
+
+## Running the Script
+
+In order to run the analysis, execute the `run_analysis.R` script from your work directory where `UCI HAR Dataset` data directory resides. OR alternatively execute the script from within the `UCI HAR Dataset` directory, both ways should work fine.
+
 ## Notes
 
-* Measures of `meanFreq` and `gravityMean` features were filtered out because the task said to only keep `mean` and `std` features and for example for `fBodyAccJerk` there are `mean`, `std` and `meanFreq` features, meaning that `meanFreq` is something else. There's a post on this [here](https://class.coursera.org/getdata-009/forum/thread?thread_id=158)
+* Measures of `meanFreq` and `gravityMean` features were filtered out because the task said to only keep `mean` and `std` features and for example for `fBodyAccJerk` there are `mean`, `std` and `meanFreq` features, meaning that `meanFreq` is something else. There's a post on this [here](https://class.coursera.org/getdata-009/forum/thread?thread_id=158). This decision is also supported by the content of `features_info.txt` file where set of variables is described.
 * Steps 1-4 were not done in order, according to the comments in the [David's Project FAQ](https://class.coursera.org/getdata-009/forum/thread?thread_id=58) the order of the steps is not critical. The reason why the order is mixed up it the design of `readAndNameData` function - it handles one directory with data at once. Of course it would have been possible to do the steps in the order as they were listed but in this case there will be more data cleanup code duplication.
+* It takes some time for the script to finish but the task didn't have any non-functional requirements for performance.
--- run_analysis.R
+++ run_analysis.R
@@ -15,10 +15,11 @@ readAndNameData <- function(dataType) {
     data.X.filename <- paste(paste("X", dataType, sep = "_"), "txt", sep = ".")
     data.X.raw <- read.table(data.X.filename, col.names = features.raw$V2)
     data.Y.filename <- paste(paste("Y", dataType, sep = "_"), "txt", sep = ".")
-    data.Y.raw <- read.table(data.Y.filename, colClasses = c("factor"), col.names = c("Activity"))    
-    levels(data.Y.raw$Activity) <- activity.labels.raw$V2    
+    data.Y.raw <- read.table(data.Y.filename, colClasses = c("factor"), col.names = c("activity"))    
+    # Label activity names (Step 3)
+    levels(data.Y.raw$activity) <- activity.labels.raw$V2    
     data.S.filename <- paste(paste("subject", dataType, sep = "_"), "txt", sep = ".")
-    data.S.raw <- read.table(data.S.filename, colClasses = c("factor"), col.names = c("Subject"))
+    data.S.raw <- read.table(data.S.filename, colClasses = c("factor"), col.names = c("subject"))
     data <- cbind(data.S.raw, data.Y.raw)
     data <- cbind(data, data.X.raw)
     setwd(orig.dir)
@@ -26,9 +27,12 @@ readAndNameData <- function(dataType) {
   }
 }
 
+# Read data from test directory
 test <- readAndNameData("test")
+# Read data from train directory
 train <- readAndNameData("train")
 
+# Merge data (step 1)
 data <- rbind(train, test)
 
 relevant.measures <- grepl("mean\\.\\.|std\\.\\.", names(data))
@@ -36,5 +40,18 @@ relevant.attributes <- names(data) %in% c("Subject", "Activity")
 relevant.variables <- relevant.measures | relevant.attributes
 
 relevant.data <- data[relevant.variables]
+
+# Acc = Accelerometer
+names(relevant.data) <- gsub("Acc", "Accelerometer", names(relevant.data))
+# Gyro = Gyroscope
+names(relevant.data) <- gsub("Gyro", "Gyroscope", names(relevant.data))
+# t = time (domain)
+names(relevant.data) <- gsub("^t", "time", names(relevant.data))
+# f = freq (domain)
+names(relevant.data) <- gsub("^f", "freq", names(relevant.data))
+# trim trailing dots
+names(relevant.data) <- gsub("\\.+$", "", names(relevant.data))
+# strip repeating dots
+names(relevant.data) <- gsub("\\.+", "\\.", names(relevant.data))
 
 setwd(orig.dir)
